0dccdb1b76ccbeb2f7c8454bafe6944c6bdccbd0
Sundar Tamang <sundartamang6@gmail.com>
2025-02-18 13:37:04 +0545

    feat: initial commit

---
+++ src/test/java/com/khata/KhataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
