675900234608a066259cceb5492474793076a792
Sundar Tamang <sundartamang6@gmail.com>
2025-02-18 22:02:55 +0545

    feat: user service implemented,updated properties

--- src/main/java/com/khata/auth/entity/enums/Role.java
+++ src/main/java/com/khata/auth/entity/enums/Role.java
@@ -2,6 +2,5 @@ package com.khata.auth.entity.enums;
 
 public enum Role {
     USER,
-    ADMIN,
-    MODERATOR
+    ADMIN
 }
--- src/main/java/com/khata/utils/AppConstants.java
+++ src/main/java/com/khata/utils/AppConstants.java
@@ -5,4 +5,6 @@ public class AppConstants {
     public static final String PAGE_SIZE = "10";
     public static final String SORT_DIR ="asc";
     public static final String SORT_BY = "id";
+
+    public static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@(.+)$";
 }
