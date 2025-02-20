f75568d09361e4a39a2afa799b06547b420a42b8
Sundar Tamang <sundartamang6@gmail.com>
2025-02-20 17:39:26 +0545

    feat: congifured custom security, userDetailSService

--- src/main/java/com/khata/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/khata/exceptions/ResourceNotFoundException.java
@@ -9,11 +9,19 @@ public class ResourceNotFoundException extends RuntimeException {
     String resourceName;
     String fieldName;
     long longValue;
+    String stringValue;
 
     public ResourceNotFoundException(String resourceName, String fieldName, long longValue){
         super(String.format("%s not found with %s : %d", resourceName, fieldName, longValue));
         this.resourceName = resourceName;
         this.fieldName = fieldName;
         this.longValue = longValue;
+    }
+
+    public ResourceNotFoundException(String resourceName, String fieldName, String stringValue){
+        super(String.format("%s not found with %s : %s", resourceName, fieldName, stringValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.stringValue = stringValue;
     }
 }
