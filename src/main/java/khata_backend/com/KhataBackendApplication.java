9a8a66bfca229c7211dc7161d4e198cf87faa300
sundartamang <sundartamang6@gmail.com>
2026-08-31 20:18:49 +0545

    feat: initial commit

---
+++ src/main/java/khata_backend/com/KhataBackendApplication.java
@@ -0,0 +1,13 @@
+package khata_backend.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhataBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhataBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/khata_backend/com/KhataBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package khata_backend.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhataBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
